1deab0a433448abda7cb0fba6209fb379ebf96ab
jiko21 <daikikojima0531@icloud.com>
2022-02-03 19:52:00 +0900

    add: sample component

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+import React, { ButtonHTMLAttributes } from 'react';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const Button: React.FC<ButtonProps> = () => {
+  return <button>aaaa</button>;
+};
